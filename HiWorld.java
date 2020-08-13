6a1e091598fa0ce04169f87668b70beabd977d14
AndrewLi-alt <57926673+AndrewLi-alt@users.noreply.github.com>
2020-08-13 12:41:37 -0700

    Add files via upload

---
+++ HiWorld.java
@@ -0,0 +1,11 @@
+public class HiWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        System.out.println("My name is Andrew Li. I am 15 years old. \nMy house is in the Northwest section of Milpitas, where all the houses are.");
+        System.out.println("I am currently in 11th grade, and usually go to sleep before 11. \nI don't usually play console video games, but I occasionally play on my Switch.\nI plan to go to any college which sports a number of job opportunities in the computer science field.");
+        System.out.println("After struggling to sleep amid constant tears and stress for the future, \nI usually wake up too early to distract myself from reality.");
+        System.out.println("I was told there were many issues at my last checkup.\nI'm trying to explain that I'm a person who lives a very pathetic and overdramatized life.");
+        System.out.println("I take care to trouble myself with stress, to make sure I do not get a good rest.\nThat is how I deal with society, and I do not know what brings anyone happiness.\nAlthough if I were to try, I wouldn't lose to anything");
+
+    }
+}
